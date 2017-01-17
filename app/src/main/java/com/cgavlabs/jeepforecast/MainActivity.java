49079666b9a5a197c92fee3abc99d53bb4023a8b
cheinz <cheinz@captechconsulting.com>
2017-01-16 20:38:39 -0500

    Set toolbar on main activity and added some style attributes to fix coloring of title in toolbar

--- app/src/main/java/com/cgavlabs/jeepforecast/MainActivity.java
+++ app/src/main/java/com/cgavlabs/jeepforecast/MainActivity.java
@@ -2,11 +2,20 @@ package com.cgavlabs.jeepforecast;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.widget.Toolbar;
 
 public class MainActivity extends AppCompatActivity {
+
+  private Toolbar toolbar;
 
   @Override protected void onCreate(Bundle savedInstanceState) {
     super.onCreate(savedInstanceState);
     setContentView(R.layout.activity_main);
+    findViews();
+    setSupportActionBar(toolbar);
+  }
+
+  private void findViews() {
+    toolbar = (Toolbar) findViewById(R.id.toolbar);
   }
 }
