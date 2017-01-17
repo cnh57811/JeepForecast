e009b920b358c59d76430a52f96fbef6b48efe64
cheinz <cheinz@captechconsulting.com>
2017-01-16 21:14:25 -0500

    added menu

--- app/src/main/java/com/cgavlabs/jeepforecast/MainActivity.java
+++ app/src/main/java/com/cgavlabs/jeepforecast/MainActivity.java
@@ -1,8 +1,10 @@
 package com.cgavlabs.jeepforecast;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -17,5 +19,15 @@ public class MainActivity extends AppCompatActivity {
 
   private void findViews() {
     toolbar = (Toolbar) findViewById(R.id.toolbar);
+  }
+
+  @Override public boolean onCreateOptionsMenu(Menu menu) {
+    getMenuInflater().inflate(R.menu.menu_main, menu);
+    return true;
+  }
+
+  @Override public boolean onOptionsItemSelected(MenuItem item) {
+    int id = item.getItemId();
+    return id == R.id.action_settings || super.onOptionsItemSelected(item);
   }
 }
