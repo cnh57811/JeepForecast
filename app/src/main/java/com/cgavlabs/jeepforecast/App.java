1cba4c15973c20705d54e815367c18d4667e508d
cheinz <cheinz@captechconsulting.com>
2017-01-26 16:24:36 -0500

    swapped out android log for timber

--- app/src/main/java/com/cgavlabs/jeepforecast/App.java
+++ app/src/main/java/com/cgavlabs/jeepforecast/App.java
@@ -3,6 +3,7 @@ package com.cgavlabs.jeepforecast;
 import android.app.Application;
 import io.realm.Realm;
 import io.realm.RealmConfiguration;
+import timber.log.Timber;
 
 public class App extends Application {
 
@@ -12,5 +13,11 @@ public class App extends Application {
     Realm.init(this);
     RealmConfiguration rc = new RealmConfiguration.Builder().deleteRealmIfMigrationNeeded().build();
     Realm.setDefaultConfiguration(rc);
+
+    Timber.plant(new Timber.DebugTree(){
+      @Override protected String createStackElementTag(StackTraceElement e) {
+        return super.createStackElementTag(e) + ":" + e.getLineNumber();
+      }
+    });
   }
 }
